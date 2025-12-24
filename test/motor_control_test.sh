2c9380fa9b5aec2ea3d367fd2825dbb1d2e18e84
Marcel <marcel@procyon.luma>
2025-12-24 16:52:36 +0100

    IP address as parameter

--- test/motor_control_test.sh
+++ test/motor_control_test.sh
@@ -4,21 +4,27 @@ function wait_seconds () {
     echo $((1 + $RANDOM % 5))
 }
 
-DRONES_IP="172.23.1.222"
+
+IP="172.23.1.222"
+
+if [ -n "$1" ]
+then
+    IP="$1"
+fi
 
 # motor init
-curl -X GET "http://$DRONES_IP/mctrl?mcmd=4&steps=300"
+curl -X GET "http://$IP/mctrl?mcmd=4&steps=300"
 sleep 20
 # anticlockwise
-curl -X GET "http://$DRONES_IP/mctrl?mcmd=-1&steps=600"
+curl -X GET "http://$IP/mctrl?mcmd=-1&steps=600"
 
 while :
 do
     # door open
-    curl -X GET "http://$DRONES_IP/mctrl?mcmd=2&steps=300"
+    curl -X GET "http://$IP/mctrl?mcmd=2&steps=300"
     sleep 10
     # door close
-    curl -X GET "http://$DRONES_IP/mctrl?mcmd=3&steps=300"
+    curl -X GET "http://$IP/mctrl?mcmd=3&steps=300"
     sleep 10
 done
 
